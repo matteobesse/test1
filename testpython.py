e27a9d67b66f74ac4a1efab9632ff7e591821cca
matteobesse <matteo.besse@deeplime.io>
2025-10-15 12:03:44 +0200

    ajout des fonctions linéaire, cube, inverse carrée et inverse cube

--- testpython.py
+++ testpython.py
@@ -2,36 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Définir l'intervalle de -10 à 10 avec 100 points
-x = np.linspace(-10, 10, 100)
-y = x ** 2
+x = np.linspace(-20, 20, 1000)
+a = x
+b = x ** 2
+c = x ** 3
+d = 1/x
+e = 1/(x**2)
+f= 1/(x**3)
 
-# Tracer la courbe pour une fonction carrée
-plt.plot(x, y, label="f(x) = x²")
+# Tracer la courbe des fonctions
+plt.plot(x, a, label="f(x) = x") #fonction linéaire
+plt.plot(x, b, label="f(x) = x²")
+plt.plot(x, c, label="f(x) = x³")
+plt.plot(x, d, label="f(x) = 1/x")
+plt.plot(x, e, label="f(x) = 1/x²")
+plt.plot(x, f, label="f(x) = 1/x³")
 
 # Ajouter un titre et des labels
-plt.title("Représentation de la fonction f(x) = x²")
+plt.title("Représentation des la fonction f(x)")
 plt.xlabel("x")
 plt.ylabel("f(x)")
+
+plt.ylim(-100,100)
 
 # Ajouter une grille et une légende
 plt.grid(True)
 plt.legend()
 
 # Afficher la figure
-#plt.show()
-
-# Pour une fonction inverse
-a = np.linspace(-10,10,100)
-b = 1/a
-
-plt.plot(a,b, label="f(x)=1/x")
-
-plt.title("f(x)=1/x")
-plt.xlabel("x")
-plt.ylabel("y")
-
-plt.grid
-plt.legend()
-
 plt.show()
-
